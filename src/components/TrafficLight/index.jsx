00169b6faef24c4c9b1eea570201b455d5f36cec
Lee Marsden <marzy@Emmas-iPhone.home>
2017-02-24 21:31:20 +0000

    Added latest

--- src/components/TrafficLight/index.jsx
+++ src/components/TrafficLight/index.jsx
@@ -1,22 +1,61 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
-import { Signal } from 'components';
 import './index.scss';
 
-const TrafficLight = (props) => {
-  const classNames = classnames('trafficLight', props.direction);
+class TrafficLight extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-  return (
-    <div className={classNames}>
-      <Signal />
-      <Signal />
-      <Signal />
-    </div>
-  );
-};
+  componentWillMount() {
+    if (this.props.direction === 'north' || this.props.direction === 'south') {
+      this.counter = 0;
+      this.setState({
+        current: this.props.allStates[this.counter],
+      });
+    } else {
+      this.counter = 2;
+      this.setState({
+        current: this.props.allStates[this.counter],
+      });
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.counter = (this.counter + 1) % 4;
+      this.setState({
+        current: this.props.allStates[this.counter],
+      });
+    }, this.state.current.time);
+  }
+
+  getClass(classToSignal) {
+    if (this.state.current.classToDisplay.indexOf(classToSignal) >= 0) {
+      return classToSignal;
+    }
+    return '';
+  }
+
+  render() {
+    const classNames = classnames('trafficLight', this.props.direction);
+    return (
+      <div className={classNames}>
+        <div className={`signal ${this.getClass('stop')}`} />
+        <div className={`signal ${this.getClass('getReady')}`} />
+        <div className={`signal ${this.getClass('go')}`} />
+      </div>
+    );
+  }
+}
 
 TrafficLight.propTypes = {
   direction: PropTypes.string.isRequired,
+  allStates: PropTypes.arrayOf(PropTypes.shape({
+    classToDisplay: PropTypes.array.isRequired,
+    time: PropTypes.number.isRequired,
+  })).isRequired,
 };
 
 export default TrafficLight;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,30 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Header, Footer, TrafficLight } from 'components';
 
 const siteName = 'Traffic Lights';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isActive: true,
-      waitForGetReady: true,
-    };
-  }
-  render() {
-    return (
-      <div>
-        <Header name={siteName} />
-        <main>
-          <TrafficLight direction="north" />
-          <TrafficLight direction="south" />
-          <TrafficLight direction="east" />
-          <TrafficLight direction="west" />
-        </main>
-        <Footer name={siteName} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const allStates = [{
+    classToDisplay: ['stop'],
+    time: 10000,
+  }, {
+    classToDisplay: ['stop', 'getReady'],
+    time: 5000,
+  }, {
+    classToDisplay: ['go'],
+    time: 10000,
+  }, {
+    classToDisplay: ['go', 'getReady'],
+    time: 5000,
+  }];
+
+  return (
+    <div>
+      <Header name={siteName} />
+      <main>
+        <TrafficLight direction="north" allStates={allStates} />
+        <TrafficLight direction="south" allStates={allStates} />
+        <TrafficLight direction="east" allStates={allStates} />
+        <TrafficLight direction="west" allStates={allStates} />
+      </main>
+      <Footer name={siteName} />
+    </div>
+  );
+};
 
 export default App;
